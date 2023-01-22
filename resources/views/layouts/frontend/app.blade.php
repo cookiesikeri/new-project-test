9f7d6894e45261f98e8b254a8315d326a22318dc
cookiesikeri <ikbenezer@gmail.com>
2023-01-22 23:47:04 +0100

    updated files

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="zxx" class="no-js">
+<html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <!-- Mobile Specific Meta -->
     <meta
@@ -17,7 +17,8 @@
     <!-- meta character set -->
     <meta charset="UTF-8" />
     <!-- Site Title -->
-    <title>Blogger</title>
+    <title>Laravel Test</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link
       href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700"
